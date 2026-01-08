b78e82deaabc12af9c327542a20fe63941601b15
C.Ponapalt <ponapalt@shillest.net>
2026-01-09 08:12:30 +0900

    v0.2.0.0

--- GhostDist/Properties/AssemblyInfo.cs
+++ GhostDist/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("GhostDist")]
-[assembly: AssemblyDescription("伺かゴースト配布系自動化システム")]
+[assembly: AssemblyDescription("ゴースト配布系自動化システム")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GhostDist")]
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
