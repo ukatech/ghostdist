87b6a0662c9cf98620d5a0c77a4f05a2e07815fc
C.Ponapalt <ponapalt@shillest.net>
2026-01-09 13:36:22 +0900

    v0.2.0.1

--- GhostDist/Properties/AssemblyInfo.cs
+++ GhostDist/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a1b2c3d4-e5f6-7890-abcd-ef1234567890")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.1")]
+[assembly: AssemblyFileVersion("0.2.0.1")]
